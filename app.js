19d908a35a93adcd54eec2831af441f970e27ffa
jesus-castro3 <jesus.castro3@gmail.com>
2019-01-22 10:24:43 -0700

    Added layout api, builds a response out of a map configuration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const Users = require('./db/models/users');
 const mapConfig = require('./constants/mapConfig');
 
@@ -19,6 +19,15 @@ const getFirstQueryParam = (query) => {
   return res;
 }
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  next();
+})
+
 app.get('/users', (req, res) => {
   const query = getFirstQueryParam(req.query);
   
@@ -28,7 +37,22 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/layout', (req, res) => {
-  return res.send(mapConfig);
-})
+  const layout = mapConfig.map((z) => {
+    const houses = z.lots.reduce((accum, lot)=> {
+      let i = 0;
+      do {
+        let houseLetter = String.fromCharCode(97+i).toUpperCase();
+        accum.push(lot.number + houseLetter);
+        i += 1;
+      } while (i < lot.repeat)
+      return accum;
+    }, []);
+    return {
+      zone: z.zone,
+      houses
+    }
+  })
+  return res.send(layout);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
